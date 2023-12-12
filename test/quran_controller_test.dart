85b4ddad461fc20ee52d07d42a149ceda380f2bf
hassan Al-khafagy <72603041+hassony105@users.noreply.github.com>
2023-12-12 09:11:56 +0300

    some changes

--- test/quran_controller_test.dart
+++ test/quran_controller_test.dart
@@ -5,14 +5,7 @@ import 'package:quran_controller/models/models.dart';
 import 'package:quran_controller/quran_controller.dart';
 
 void main() {
-  test('adds one to input values', () async {
-    // WidgetsFlutterBinding.ensureInitialized();
-    // QuranController quranController = QuranController(pageNumber: 1);
-    // await quranController.initializing();
-    // expect(quranController.verses.verses?.length, 7);
-    // final calculator = Calculator();
-    // expect(calculator.addOne(2), 3);
-    // expect(calculator.addOne(-7), -6);
-    // expect(calculator.addOne(0), 1);
+  test('test', () async {
+
   });
 }
